3879386f483142a9c76137f34d5850808f1fe3ea
Carlos Vieira <vieiramecarlos@gmail.com>
2018-12-05 11:16:38 -0300

    Fix ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,22 +1,17 @@
-df = pd.read_csv("data/all-data-ready.csv", header=None)
-pca = PCA(0.96)
-pca.fit(X)
-print(pca.explained_variance_ratio_)
-base_classifiers(pca.transform(X), y, pca.transform(X_val), y_val)
-
 import pandas as pd
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 _random_seed = 42
 
-def get_data():
-	df = pd.read_csv("data/all-data-ready.csv", header=None)
+def _get_data():
+	df = pd.read_csv("data/all-data-ready.csv", index_col=0)
 	X, y = df.drop(['heuristic'], axis=1).astype('float64'), df['heuristic']
 	return X, y
 
 def get_raw():
-	X, y = get_data()
+	X, y = _get_data()
 
 	X, X_test, y, y_test = train_test_split(
 	    X, y, stratify=y, test_size=0.1, random_state=_random_seed)
@@ -30,4 +25,6 @@ def get_pca():
 	X, X_test, y, y_test = get_raw()
 	pca_ = PCA(0.96)
 	pca_.fit(X)
-	return pca_.transform(X), pca_.transform(X_test), y, y_test
+	X_pca = pd.DataFrame(pca_.transform(X))
+	X_test_pca = pd.DataFrame(pca_.transform(X_test))
+	return X_pca, X_test_pca, y, y_test
