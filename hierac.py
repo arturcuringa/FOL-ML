da8a3fa343b0c1e7dfabc5a34234f7d179b210dc
Carlos Vieira <vieiramecarlos@gmail.com>
2018-12-05 18:18:53 -0300

    Update unsupervised scripts

--- hierac.py
+++ hierac.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from ingestion import get_raw, get_pca
@@ -9,6 +10,7 @@ import seaborn as sns
 
 sns.set(style="whitegrid")
 warnings.filterwarnings('ignore')
+os.makedirs("results/hierac", exist_ok=True)
 
 X, X_test, y, y_test = get_raw()
 attr_sz = len(np.unique(y))
@@ -22,8 +24,8 @@ for k in range(2,13):
 	k_dict[k] = davies_bouldin_score(X,labels)
 
 df = pd.DataFrame.from_dict(k_dict, orient='index')
-df.to_csv('hierac.csv')
+df.to_csv('results/hierac/hierac.csv')
 
 ax = sns.lineplot(x=list(k_dict.keys()), y=list(k_dict.values()))
 ax.set(xlabel = 'k', ylabel = 'DB')
-plt.savefig('hierac.png')
+plt.savefig('results/hierac/hierac.png')
--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from ingestion import get_raw, get_pca
@@ -9,6 +10,7 @@ import seaborn as sns
 
 sns.set(style="whitegrid")
 warnings.filterwarnings('ignore')
+os.makedirs("results/kmeans", exist_ok=True)
 
 X, X_test, y, y_test = get_raw()
 attr_sz = len(np.unique(y))
@@ -25,7 +27,7 @@ for k in range(2,13):
 	k_dict[k] = np.array(db_values).sum()/5
 
 df = pd.DataFrame.from_dict(k_dict, orient='index')
-df.to_csv('kmeans.csv')
+df.to_csv('results/kmeans/kmeans.csv')
 ax = sns.lineplot(x=list(k_dict.keys()), y=list(k_dict.values()))
 ax.set(xlabel = 'k', ylabel = 'DB')
-plt.savefig('kmeans.png')
+plt.savefig('results/kmeans/kmeans.png')
