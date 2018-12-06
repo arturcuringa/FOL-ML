60ff1fe051a148e1ea766318ac5af6147698047b
Artur Curinga <arturmcuringa@gmail.com>
2018-12-06 01:29:35 -0300

    Add new results

--- kmeans_pca.py
+++ kmeans_pca.py
@@ -1,10 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
-from ingestion import get_raw, get_pca ,get_data
+from ingestion import get_raw, get_pca, get_full_pca
 from sklearn.cluster import KMeans
 from sklearn.metrics import davies_bouldin_score
-from sklearn.preprocessing import StandardScaler
 import warnings
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,10 +12,7 @@ sns.set(style="whitegrid")
 warnings.filterwarnings('ignore')
 os.makedirs("results/kmeans", exist_ok=True)
 
-X,y = get_data()
-scaler = StandardScaler()
-X = pd.DataFrame(scaler.fit_transform(X))
-
+X, y = get_full_pca()
 attr_sz = len(np.unique(y))
 print(attr_sz)
 k_dict = {}
@@ -31,7 +27,7 @@ for k in range(2,13):
 	k_dict[k] = np.array(db_values).sum()/5
 
 df = pd.DataFrame.from_dict(k_dict, orient='index')
-df.to_csv('results/kmeans/kmeans.csv')
+df.to_csv('results/kmeans/kmeans_PCA.csv')
 ax = sns.lineplot(x=list(k_dict.keys()), y=list(k_dict.values()))
 ax.set(xlabel = 'k', ylabel = 'DB')
-plt.savefig('results/kmeans/kmeans.png')
+plt.savefig('results/kmeans/kmeans_PCA.png')
---
+++ mlp2.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+import numpy as np
+from ingestion import get_raw, get_pca
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+
+
+os.makedirs("results/mlp", exist_ok=True)
+
+def mlp(ingest, scale):
+
+    X, X_test, y, y_test = ingest()
+
+    params = {
+            'classifier__hidden_layer_sizes' : [(46,)],        
+            'classifier__max_iter' :  [1000],
+            'classifier__learning_rate_init' : [0.01]
+    }
+    pipe_steps = []
+    if scale:
+        pipe_steps.append(('scale', StandardScaler()))
+    pipe_steps.append(('classifier', MLPClassifier(solver='sgd',momentum= 0.8,nesterovs_momentum= False)))
+    pipe = Pipeline(pipe_steps)
+    
+    kfold = StratifiedKFold(10, shuffle=True)
+    cv_grid = GridSearchCV(pipe, params, scoring='accuracy',return_train_score=True, cv=kfold, verbose=1, n_jobs=-1)
+    cv_grid.fit(X,y)
+
+    cv_result = pd.DataFrame(cv_grid.cv_results_)
+
+    test_scores = []
+    for params in cv_result["params"]:
+        pipe = pipe.set_params(**params).fit(X, y)
+        y_pred = pipe.predict(X_test)
+
+        score = accuracy_score(y_test, y_pred)
+        test_scores.append(score)
+
+    cv_result['holdout_test_score'] = pd.Series(
+        test_scores, index=cv_result.index)
+
+    return cv_result
+
+datasets = [('raw', get_raw), ('pca', get_pca)]
+for data_name, data_func in datasets:
+    for scale in (True, False):
+        for i in range(10):
+            result = mlp(data_func, scale)
+            filename = "mlp_result_" + data_name + "_"
+            filename += "scaled" if scale else "not_scaled"
+            filename += "_"+str(i)
+            result.to_csv("results/mlp/" + filename + ".csv")
+            print(filename, "done")
+
